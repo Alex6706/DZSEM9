e9cb4f5cf436bfb387cd4764c3331b68adc70eed
Alex <122726581+Alex6706@users.noreply.github.com>
2023-02-03 20:39:26 +0300

    Update Program.cs

--- Z66/Program.cs
+++ Z66/Program.cs
@@ -1,1 +1,23 @@
+//Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
+int SumNumbers (int from, int to)
+{
+    if (from == to)
+    {
+        return from;
+    }
+    var direction = to > from ? 1 : -1;
+    return SumNumbers (from + direction, to) + from;
+}
+
+Console.Write ("Введите первое число: ");
+int firstNumber = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+int secondNumber = Convert.ToInt32 (Console.ReadLine());
+
+int sum = SumNumbers (firstNumber, secondNumber);
+Console.WriteLine ($"Сумма натуральных элементов в промежутке от M до N: {sum}");
